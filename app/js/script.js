2db99d9fcce37d2898f7e57e7fb94fbb4a810e11
MarouaneBenchiekh <marouanebenchiekh@gmail.com>
2021-10-14 23:12:28 +0000

    Add slider for the product

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,4 @@
+//Mobile menu
 const menu= document.getElementById("menu");
 const overlay= document.getElementById("overlay");
 
@@ -13,3 +14,37 @@ document.getElementById("humburger").addEventListener("click", function (){
         overlay.classList.add("visible-overlay");
     }
 })
+
+//slider
+const items = document.querySelectorAll(".slider");
+const nbSlide = items.length;
+const next = document.getElementById("next");
+const previous = document.getElementById("previous");
+let count = 0;
+console.log(next);
+
+function nextSlide(){
+    items[count].classList.remove('active');
+    if(count < nbSlide - 1){
+        count++;
+    }
+    else {
+        count = 0;
+    }
+    items[count].classList.add('active');
+    console.log("ksjios")
+}
+next.addEventListener("click", nextSlide)
+
+function previousSlide(){
+    items[count].classList.remove('active');
+    if(count > 0){
+        count--;
+    }
+    else {
+        count = nbSlide-1;
+    }
+    items[count].classList.add('active');
+    console.log("ksjios")
+}
+previous.addEventListener("click", previousSlide)
